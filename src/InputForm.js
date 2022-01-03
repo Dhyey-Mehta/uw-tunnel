6acacecd6b4c436485fd1cd467319f6c3d0aff42
Dhyey Mehta <dhyey.p.mehta@gmail.com>
2022-01-03 00:31:28 -0500

    Added cards to display on screen when submit button pressed

--- src/InputForm.js
+++ src/InputForm.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import axios from "axios";
 import Button from '@mui/material/Button';
 import TextInput from "./TextInput";
-import makeCards from "./makeCards"
+import MakeCards from "./makeCards"
 import "./App.css";
 
 const options = ["B1","B2","C2","DC","E3","E5","EIT","ESC","M3","MC","PAC","QNC","SLC"]
@@ -12,31 +12,38 @@ const InputForm = () =>{
     const [userInStart,setUserInStart] = useState(options[0]);
     const [valEnd,setValEnd] = useState("");
     const [userInEnd,setUserInEnd] = useState(options[0]);
+    const [completed,setCompleted] = useState({done:false,path:[]});
 
     const submitForm = (event)=>{
         //Make sure that the page does not refresh when submit form is clicked
         event.preventDefault();
         //Replace with actual API link in prod
         const apiURL = `http://localhost:4040/route/${valStart}/${valEnd}`;
-
         axios.get(apiURL).then(
             (res)=>{
-                console.log(res.data.path);
+                const pathFound = res.data.path;
+                console.log(pathFound);
+                setCompleted({done:true, path:pathFound});
             }
         ).catch((err)=>{
             console.log(err);
         })
     }
 
-    return(
-        <div>
-            <form onSubmit={submitForm}>
-                <TextInput innerText="From" val={valStart} setVal={setValStart} userIn={userInStart} setUserIn={setUserInStart} options={options} className="text-box" />
-                <TextInput innerText="To" val={valEnd} setVal={setValEnd} userIn={userInEnd} setUserIn={setUserInEnd} options={options} className="text-box"/>
-                <Button sx={{marginTop:2}} size="medium" type="submit" varient="outlined" colour="primary" onClick={submitForm} className="centered" > Submit </Button>
-            </form> 
-        </div>
-    );
+    if (completed.done){
+        return (<MakeCards pathLst={completed.path} />);
+    }
+    else{
+        return(
+            <div>
+                <form onSubmit={submitForm}>
+                    <TextInput innerText="From" val={valStart} setVal={setValStart} userIn={userInStart} setUserIn={setUserInStart} options={options} className="text-box" />
+                    <TextInput innerText="To" val={valEnd} setVal={setValEnd} userIn={userInEnd} setUserIn={setUserInEnd} options={options} className="text-box"/>
+                    <Button sx={{marginTop:2}} size="medium" type="submit" varient="outlined" colour="primary" onClick={submitForm} className="centered" > Submit </Button>
+                </form> 
+            </div>
+        );
+    }
 }
 
 export default InputForm;
